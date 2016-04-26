17b60dda14b5d677359626c99b7303ba1365c523
wagneresser <waesser@gmail.com>
2016-04-26 14:09:44 -0300

    Resolvido o problema do formulário de respostas às perguntas

--- rango/views.py
+++ rango/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect, HttpResponse #, Http404
+# from django.urls import reverse
+from django.core.urlresolvers import reverse
 from rango.models import Category, Page, Question, Choice
 from rango.forms import CategoryForm
 
@@ -49,7 +51,22 @@ def results(request, question_id):
     return HttpResponse(response % question_id)
 
 def vote(request, question_id):
-    return HttpResponse("Voce esta votando na questao %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'rango/detail.html', {
+            'question': question,
+            'error_message': "Deve selecionar uma escolha!",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('rango:results', args=(question.id,)))
 
 def add_category(request):
     # A HTTP POST?
