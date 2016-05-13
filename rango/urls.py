5133489523f8cccbe9f4540f6bf68f8a5aed47db
wagneresser <waesser@gmail.com>
2016-05-13 16:41:38 -0300

    Adição de modelo base (templates/rango/base.html); Mudança da página 'restricted' para um modelo (restricted.html).

--- rango/urls.py
+++ rango/urls.py
@@ -4,13 +4,13 @@ from django.views.generic import TemplateView
 
 urlpatterns  =  [
     url(r'^$', views.index, name='index'),
+
+	# Tango Django
     url(r'^add_user$', views.add_user, name="add_user"),
     url(r'^register/$', views.register, name='register'),
     url(r'^login/$', views.user_login, name='login'),
     url(r'^restricted/$', views.restricted, name='restricted'),
     url(r'^logout/$', views.user_logout, name='logout'),
-
-	# Tango Django
     url(r'^about/', TemplateView.as_view(template_name="rango/about.html"), name='about'),
     url(r'^categories/', views.categories, name='categories'),
     url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.category, name='category'),
--- rango/views.py
+++ rango/views.py
@@ -11,7 +11,7 @@ from django.contrib.auth import logout
 
 @login_required
 def restricted(request):
-    return HttpResponse("Since you're logged in, you can see this text!")
+    return render(request, 'rango/restricted.html', {})
 
 @login_required
 def user_logout(request):
