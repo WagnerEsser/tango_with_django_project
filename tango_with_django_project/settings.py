32c6b53cbbf971bff841d4635cdbc543d6e8927c
wagneresser <waesser@gmail.com>
2016-12-14 01:56:35 -0200

    Correção de BUG no módulo 'security' na linha 65 - settings.py

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -62,7 +62,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.security.SecurityMiddleware',
+    # 'django.middleware.security.SecurityMiddleware',
 )
 
 ROOT_URLCONF = 'tango_with_django_project.urls'
